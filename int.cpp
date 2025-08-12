c6f13f8fe3b0a8fb4099be02b8b2357409a3fcca
Is0c <isaacpazb08@gmail.com>
2025-08-12 10:58:47 -0300

    Add files via upload

---
+++ int.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string.h>
+#include <vector>
+using namespace std;
+int main() {
+    vector<int> num={13,40,36,68,77,51,45,90};
+    int cantidad;
+    int valor;
+    cout<<"mostrar cantidad de cada vector";
+    cin>>cantidad;
+    for (int i=1; i>50; i++)
+    {
+        for(int j=1; j>50; i++)
+        {
+            if(valor>=cantidad)
+            {
+                cout<<"mostrar los vectores que son mayores a 50";   
+            }
+            else{
+                cout<<"mostrar los son mayores a 36";
+            }   
+        }   
+    }
+return 0;
+}
